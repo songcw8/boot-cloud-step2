30125739fea7f2a533376f4ba7bfdda395d9c615
songcw8 <ben990158@gmail.com>
2025-05-28 15:29:05 +0900

    Initial commit

---
+++ src/main/java/org/example/bootcloudstep2/BootCloudStep2Application.java
@@ -0,0 +1,13 @@
+package org.example.bootcloudstep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCloudStep2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCloudStep2Application.class, args);
+    }
+
+}
